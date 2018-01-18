fc4916aa34cda924432a05807c06be4b9ffc07cf
Garth Poitras <411908+gpoitch@users.noreply.github.com>
2018-01-18 00:08:41 -0500

    Create proxy api gateway endpoints on init (#3)

--- src/init.js
+++ src/init.js
@@ -16,7 +16,7 @@ async function makeSamTemplate(name, opts) {
   const accountId = await getAccountId()
   const template = JSON.parse(JSON.stringify(samTemplate))
   const { Resources, Parameters } = template
-  const resourceName = `${name.charAt(0).toUpperCase() + name.slice(1)}Function`
+  const resourceName = `${name.charAt(0).toUpperCase() + name.slice(1).replace(/[\W_]+/g, '')}Function`
   const resource = (Resources[resourceName] = Resources.__RESOURCE_NAME__)
   resource.Properties.FunctionName = name
   Parameters.BucketName.Default = `sam-uploads-${accountId}`
@@ -32,14 +32,14 @@ async function makeSamTemplate(name, opts) {
 
 async function makeLambda(name) {
   const path = 'index.js'
-  const func = lambdaTemplate.replace(/__NAME__/, name)
+  const func = lambdaTemplate.replace(/__NAME__/g, name)
   await writeFileAsync(path, func)
   return path
 }
 
 async function init(name, opts) {
   return Promise.all([makeSamTemplate(name, opts), makeLambda(name)])
-    .then(files => console.log('SAM files created:', ...files)) // eslint-disable-line no-console
+    .then(files => console.log('[sammie] created files:\n', files.join(' \n '))) // eslint-disable-line no-console
     .catch(e => console.log(e.message)) // eslint-disable-line no-console
 }
 
