e3e827a7be92488b0ec9e6c9b90d0fa3c3a1c6e0
Garth Poitras <garth22@gmail.com>
2018-04-26 20:07:38 -0400

    Default to node 8 on lambda. Bump version

--- src/templates/lambda-template.js
+++ src/templates/lambda-template.js
@@ -1,5 +1,5 @@
-const template = (event, context, callback) => {
-  callback(null, {
+const template = (event, context) => {
+  return Promise.resolve({
     body: 'Hello __NAME__!',
     statusCode: 200
   })
