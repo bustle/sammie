b0cdd63982a51e283a6f6d18ee0acb9fc4bbafc5
Garth Poitras <garth22@gmail.com>
2018-05-19 09:58:47 -0400

    Display more info in default template

--- src/templates/lambda-template.js
+++ src/templates/lambda-template.js
@@ -1,8 +1,16 @@
 const template = (event, context) => {
-  return Promise.resolve({
-    body: 'Hello __NAME__!',
-    statusCode: 200
-  })
+  const body = `
+    <h1>Hello __NAME__!</h1>
+    <details>
+      <summary>Event</summary>
+      <pre>${JSON.stringify(event, null, 2)}</pre>
+    </details>
+    <details>
+      <summary>Context</summary>
+      <pre>${JSON.stringify(context, null, 2)}</pre>
+    </details>`
+
+  return Promise.resolve({ statusCode: 200, headers: { 'content-type': 'text/html' }, body })
 }
 
 module.exports = 'exports.handler = ' + template.toString() + '\n'
