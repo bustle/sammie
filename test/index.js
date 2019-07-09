d192a810c9131d335de963aba5de917c71560849
gpoitch <garth22@gmail.com>
2019-07-09 12:34:18 -0400

    Upgrade deps. Bump version

---
+++ test/index.js
@@ -0,0 +1,12 @@
+const assert = require('assert')
+const sammie = require('../src/index')
+
+process.on('unhandledRejection', e => {
+  throw e
+})
+
+describe('sammie', () => {
+  it('exists', () => {
+    assert.ok(sammie)
+  })
+})
